792fbd8e444d04698cb4e67c321f92d3e93b77fb
John Faber Flórez <john.florez25@gmail.com>
2021-01-27 16:48:17 -0500

    Created input todo component

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+//componentes
+import InputTodo from './components/InputTodo';
+
+function App() {
+  return (
+    <>
+      <div className='container'>
+        <InputTodo />
+      </div>
+    </>
+  );
+}
+
+export default App;

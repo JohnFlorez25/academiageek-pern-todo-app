1ff46b78dffc828d27cf94ee9b8bf2bc471f86d2
John Faber Flórez <john.florez25@gmail.com>
2021-01-31 20:49:24 -0500

    Updated database squema

--- database.sql
+++ database.sql
@@ -8,13 +8,27 @@
  - psql -U postgres
  */
 
-CREATE DATABASE perntodo;
+CREATE DATABASE authtodo;
+
+CREATE TABLE users(
+    user_id uuid DEFAULT uuid_generate_v4(),
+    user_name VARCHAR(255) NOT NULL,
+    user_email VARCHAR(255) NOT NULL UNIQUE,
+    user_password VARCHAR(255) NOT NULL,
+    PRIMARY KEY(user_id)
+);
 
 CREATE TABLE todo(
-    todo_id SERIAL PRIMARY KEY,
-    description VARCHAR(255)
+    todo_id SERIAL,
+    user_id UUID,
+    description VARCHAR(255),
+    description VARCHAR(255) NOT NULL,
+    PRIMARY KEY (todo_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
 SELECT * FROM todo;
+
+INSER INTO users (user_name, user_email, user_password) VALUES ('henry', 'henry123@gmail.com', 'kthl8822');
 
 INSERT INTO todo(description) VALUES ('Create meet with Alirio');
